200bfb4d6274dfeff51591aaace10bdc33786484
astkout <astkout12@hotmail.com>
2023-11-09 15:46:19 +0000

    cmt14

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -3,9 +3,10 @@ import AnimatedLetters from "../AnimatedLetters";
 import ProjectItem from "../ProjectItem";
 import Loader from "react-loaders";
 import tindog from "../../assets/images/tindog.png";
+import keeper from "../../assets/images/keeper.png";
 import drumkit from "../../assets/images/drumkit.png";
-import simon from "../../assets/images/simon.png"
 import "../Projects/index.scss";
+
 
 const Projects = () => {
   const [letterClass, setLetterClass] = useState('text-animate');
@@ -31,9 +32,8 @@ const Projects = () => {
         <div className="projects">
           <div className="projectList">
             <ProjectItem name="Tindog" image={tindog} link="https://tindog-pffl-astkout.vercel.app/"/>
-            <ProjectItem name="Drumkit" image={drumkit} link="https://drum-kit-nine-liard.vercel.app/"/>
-            <ProjectItem name="Simon Says" image={simon} link="https://simonsays-hazel.vercel.app/"/>
             <ProjectItem name="Keeper" image={keeper} link="https://keeper-git-main-astkout.vercel.app/"/>
+            <ProjectItem name="Drumkit" image={drumkit} link="https://drum-kit-nine-liard.vercel.app/"/>
           </div>
         </div>
       </div>
